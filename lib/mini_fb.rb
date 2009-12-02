2e5bbdbc98d3ca1b9091d91b93e5a857f1cecf04
Aaron <aaron@piqqem.com>
2009-12-01 16:17:13 -0800

    Require json/pure only if JSON is not already defined. Prevents errors of re-initalized constants State and JSON_LOADED.

--- lib/mini_fb.rb
+++ lib/mini_fb.rb
@@ -1,7 +1,7 @@
 module MiniFB
     require 'digest/md5'
     require 'erb'
-    require 'json/pure'
+    require 'json/pure' unless defined? ActiveSupport::JSON
 
     # Global constants
     FB_URL = "http://api.facebook.com/restserver.php"
