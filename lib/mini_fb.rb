2b5659b8dfe003def3542f9f328a6fa01d1c5c6e
dpree <whiterabbit.init@gmail.com>
2011-01-20 18:07:11 +0100

    check logging before puts

--- lib/mini_fb.rb
+++ lib/mini_fb.rb
@@ -675,7 +675,7 @@ module MiniFB
 
             return res_hash
         rescue RestClient::Exception => ex
-            puts "ex.http_code=" + ex.http_code.to_s
+            puts "ex.http_code=" + ex.http_code.to_s if @logging
             puts 'ex.http_body=' + ex.http_body if @@logging
             res_hash = JSON.parse(ex.http_body) # probably should ensure it has a good response
             raise MiniFB::FaceBookError.new(ex.http_code, "#{res_hash["error"]["type"]}: #{res_hash["error"]["message"]}")
